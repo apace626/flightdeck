eee00ba88d3a900af09f0fd060527135c4f2a1f0
Andy Pace <pace.andy@gmail.com>
2026-06-07 20:25:34 -0500

    Icon: navy-blue + gold FD (Blue Angels)

--- Scripts/make-icon.swift
+++ Scripts/make-icon.swift
@@ -12,40 +12,24 @@ let image = NSImage(size: NSSize(width: size, height: size))
 image.lockFocus()
 let ctx = NSGraphicsContext.current!.cgContext
 
-// --- Army-green camouflage background squircle ---
+// --- Navy-blue background squircle (Blue Angels) ---
 let margin = size * 0.085
 let bg = CGRect(x: margin, y: margin, width: size - margin * 2, height: size - margin * 2)
 ctx.saveGState()
 ctx.addPath(CGPath(roundedRect: bg, cornerWidth: bg.width * 0.2237, cornerHeight: bg.width * 0.2237, transform: nil))
 ctx.clip()
-
-// olive base
-ctx.setFillColor(c(74, 86, 46).cgColor)
-ctx.fill(bg)
-
-// soft camo patches (radial blobs in varied military greens)
-let patches: [(CGFloat, CGFloat, CGFloat, NSColor)] = [
-    (0.28, 0.72, 0.40, c(54, 66, 30)),    // dark olive
-    (0.74, 0.58, 0.46, c(108, 120, 66)),  // light olive
-    (0.58, 0.26, 0.34, c(43, 52, 24)),    // deep green
-    (0.18, 0.34, 0.32, c(125, 122, 78)),  // khaki
-    (0.82, 0.86, 0.30, c(92, 104, 54)),
-    (0.40, 0.48, 0.26, c(60, 74, 36)),
-]
-for (x, y, rad, col) in patches {
-    let g = CGGradient(colorsSpace: CGColorSpaceCreateDeviceRGB(),
-                       colors: [col.withAlphaComponent(0.9).cgColor, col.withAlphaComponent(0).cgColor] as CFArray,
-                       locations: [0, 1])!
-    let center = CGPoint(x: size * x, y: size * y)
-    ctx.drawRadialGradient(g, startCenter: center, startRadius: 0,
-                           endCenter: center, endRadius: size * rad, options: [])
-}
+let navy = CGGradient(colorsSpace: CGColorSpaceCreateDeviceRGB(),
+                      colors: [c(0, 24, 84).cgColor, c(10, 46, 130).cgColor] as CFArray,
+                      locations: [0, 1])!
+ctx.drawLinearGradient(navy, start: CGPoint(x: margin, y: size - margin),
+                       end: CGPoint(x: size - margin, y: margin), options: [])
 ctx.restoreGState()
 
 // --- Big bold white "FD", centered ---
 let letter = "FD"
 let font = NSFont.systemFont(ofSize: size * 0.40, weight: .heavy)
-let attrs: [NSAttributedString.Key: Any] = [.font: font, .foregroundColor: NSColor.white]
+let gold = c(255, 199, 44)   // Blue Angels gold
+let attrs: [NSAttributedString.Key: Any] = [.font: font, .foregroundColor: gold]
 let str = NSAttributedString(string: letter, attributes: attrs)
 let textSize = str.size()
 str.draw(at: NSPoint(x: (size - textSize.width) / 2,
